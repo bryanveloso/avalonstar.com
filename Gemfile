6e5894562f01943cb40640445c29af29d2190b3d
Bryan Veloso <bryan@revyver.com>
2013-12-13 13:15:26 -0800

    Add Autoprefixer.

--- Gemfile
+++ Gemfile
@@ -8,9 +8,11 @@ group :development do
   gem 'stringex', '~> 2.1.2'
   gem 'foreman', '~> 0.63.0'
 
+  gem 'autoprefixer-rails', '~> 0.8.20131213'
   gem 'coffee-script', '~> 2.2.0'
   gem 'compass', '~> 0.12.2'
   gem 'compass-normalize', '~> 1.4.3'
+  gem 'csso-rails', '~> 0.3.2'
   gem 'kramdown', '~> 1.3.0'
   gem 'liquid', '~> 2.5.2'
   gem 'susy', '~> 1.0.8'
--- config.rb
+++ config.rb
@@ -1,23 +1,33 @@
 # Require any additional compass plugins here.
-require "compass-normalize"
-require "susy"
+require 'autoprefixer-rails'
+require 'compass-normalize'
+require 'csso'
+require 'susy'
 
 # Additional import paths for external dependencies:
-add_import_path "components/typeplate/scss/"
+add_import_path 'components/typeplate/scss/'
 
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "_source/stylesheets"
+http_path = '/'
+http_images_path = '/images'
+http_fonts_path = '/fonts'
+css_dir = '_source/stylesheets'
 
 # Local development paths
-sass_dir = "sass"
-images_dir = "_source/images"
-fonts_dir = "_source/fonts"
+sass_dir = 'sass'
+images_dir = '_source/images'
+fonts_dir = '_source/fonts'
 
 line_comments = false
 output_style = :compressed
 preferred_syntax = :sass
+
+# Post-compile hoook to autoprefix our CSS.
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << Csso.optimize(AutoprefixerRails.compile(css))
+  end
+end
