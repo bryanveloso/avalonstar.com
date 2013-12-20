03b25889bdd9a9e5fd4e5824c1c3a747f2c91443
Bryan Veloso <bryan@revyver.com>
2013-12-19 20:27:23 -0800

    Single quotes.

--- config.rb
+++ config.rb
@@ -44,7 +44,7 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-page "/feed.xml", :layout => false
+page '/feed.xml', :layout => false
 
 ###
 # Helpers
