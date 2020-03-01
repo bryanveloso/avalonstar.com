500d5beea4c0c15f2983dd758463f31451aa390c
Bryan Veloso <bryan@avalonstar.com>
2020-03-01 10:32:06 -0800

    chore: Add gatsby-source-sanity configuration.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "5k911b60",
+        dataset: "production",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
