72caae887a1ab4970e076ea36590f31a567ef8d9
Bryan Veloso <bryan@avalonstar.com>
2020-03-02 13:42:12 -0800

    chore: Copy over Sanity's default Gatsby page generation module.

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { isFuture, format, parseISO } = require('date-fns')
 
-// You can delete this file if you're not using it
+async function createEntryPages(graphql, actions) {
+  const { createPage } = actions
+  const result = await graphql(`
+    allSanityEntry(
+      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+    ) {
+      edges {
+        node {
+          id
+          publishedAt
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const entryEdges = (result.data.allSanityEntry || {}).edges || []
+
+  entryEdges
+    .filter(edge => !isFuture(edge.node.publishedAt))
+    .forEach((edge, index) => {
+      const { id, slug = {}, publishedAt } = edge.node
+      const dateSegment = format(parseISO(publishedAt), 'yyyy')
+      const path = `/blog/${dateSegment}/${slug.current}`
+
+      createPage({
+        path,
+        component: require.resolve('./src/templates/entry.js'),
+        context: { id },
+      })
+    })
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  await createEntryPages(graphql, actions)
+}
