425cf55acb97098a7a7ffc4533176e7bbe99e75a
Bryan Veloso <bryan@avalonstar.com>
2020-03-03 07:21:43 -0800

    feat: Add social handles to author schema.

--- studio/schemas/documents/author.js
+++ studio/schemas/documents/author.js
@@ -1,39 +1,76 @@
 export default {
-  name: "author",
-  title: "Author",
-  type: "document",
-  __experimental_actions: ["update", /* 'create', 'delete', */ "publish"],
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  __experimental_actions: ['update', /* 'create', 'delete', */ 'publish'],
+  fieldsets: [{ name: 'social', title: 'Social Networks' }],
   fields: [
     {
-      name: "name",
-      title: "Name",
-      type: "string"
+      name: 'name',
+      title: 'Name',
+      type: 'string',
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
       options: {
-        source: "name",
-        maxLength: 96
-      }
+        source: 'name',
+        maxLength: 96,
+      },
     },
     {
-      name: "bio",
-      title: "Biography",
-      type: "array",
+      name: 'bio',
+      title: 'Biography',
+      type: 'array',
       of: [
         {
-          title: "Block",
-          type: "block"
-        }
-      ]
-    }
+          title: 'Block',
+          type: 'block',
+        },
+      ],
+    },
+    {
+      name: 'facebook',
+      title: 'Facebook',
+      type: 'url',
+      fieldset: 'social',
+    },
+    {
+      name: 'github',
+      title: 'GitHub',
+      type: 'url',
+      fieldset: 'social',
+    },
+    {
+      name: 'instagram',
+      title: 'Instagram',
+      type: 'url',
+      fieldset: 'social',
+    },
+    {
+      name: 'twitch',
+      title: 'Twitch',
+      type: 'url',
+      fieldset: 'social',
+    },
+    {
+      name: 'twitter',
+      title: 'Twitter',
+      type: 'url',
+      fieldset: 'social',
+    },
+    {
+      name: 'youtube',
+      title: 'YouTube',
+      type: 'url',
+      fieldset: 'social',
+    },
   ],
   preview: {
     select: {
-      title: "name",
-      subtitle: "slug.current"
-    }
-  }
-};
+      title: 'name',
+      subtitle: 'slug.current',
+    },
+  },
+}
