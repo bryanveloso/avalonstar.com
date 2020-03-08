1034be4a567f2b6fa2ed89d521f3d3602f4c2e69
Bryan Veloso <bryan@avalonstar.com>
2020-03-08 11:42:25 -0700

    chore: Add function to create pages for routes.

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -40,8 +40,44 @@ async function createEntryPages(graphql, actions) {
     })
 }
 
+async function createRoutePages(graphql, actions) {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allSanityRoute(filter: { slug: { current: { ne: null } } }) {
+        edges {
+          node {
+            id
+            heading
+            subheading
+            title
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const routeEdges = (result.data.allSanityRoute || {}).edges || []
+
+  routeEdges
+    .forEach((edge, index) => {
+      const { id, slug = {}, title, heading, subheading } = edge.node
+      createPage({
+        path: `/${slug.current}`,
+        component: require.resolve('./src/templates/base-page.template.tsx'),
+        context: { id, title, heading, subheading },
+      })
+    })
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   await createEntryPages(graphql, actions)
+  await createRoutePages(graphql, actions)
 }
 
 exports.onCreateWebpackConfig = ({ actions }) => {
