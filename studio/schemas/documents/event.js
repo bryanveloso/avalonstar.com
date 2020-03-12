cd50ca9308fe94380e4e8a9723cdd017ff113163
Bryan Veloso <bryan@avalonstar.com>
2020-03-12 00:17:01 -0700

    chore: Add Google Maps support to Sanity Studio.

--- studio/schemas/documents/event.js
+++ studio/schemas/documents/event.js
@@ -31,6 +31,11 @@ export default {
       title: 'Body',
       type: 'bodyPortableText',
     },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'geopoint',
+    },
   ],
   preview: {
     select: {
