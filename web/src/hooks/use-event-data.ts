5a61a44739d2752e84d8d296ca01d750af81b2fa
Bryan Veloso <bryan@avalonstar.com>
2020-03-15 16:39:05 -0700

    fix: Massage event data.

--- web/src/hooks/use-event-data.ts
+++ web/src/hooks/use-event-data.ts
@@ -1,14 +1,14 @@
 import { useStaticQuery, graphql } from 'gatsby'
 
 const QUERY = graphql`
-  query HistoryData {
-    allSanityEvent(sort: { fields: date, order: DESC }) {
+  query EventData {
+    allSanityEvent(sort: { fields: date, order: ASC }) {
       edges {
         node {
           id
           _rawBody(resolveReferences: { maxDepth: 5 })
           name
-          date
+          date(formatString: "MMMM DD, YYYY")
           subject
         }
       }
@@ -18,5 +18,11 @@ const QUERY = graphql`
 
 export const useEventData = () => {
   const { allSanityEvent } = useStaticQuery(QUERY)
-  return allSanityEvent.edges
+
+  const edges = []
+  allSanityEvent.edges.forEach(edge => {
+    const o = Object.values(edge)
+    edges.push(o)
+  })
+  return [].concat(...edges)
 }
