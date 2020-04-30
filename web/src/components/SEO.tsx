122fbef4ecd50b823ce9b2d2909b13529d1e7130
Bryan Veloso <bryan@avalonstar.com>
2020-04-30 09:29:37 -0700

    fix: Change breaking react-helmet import statement.

--- web/src/components/SEO.tsx
+++ web/src/components/SEO.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
 const QUERY = graphql`
@@ -24,7 +24,6 @@ const QUERY = graphql`
 
 function SEO({ description, lang, meta, title }) {
   const { sanitySiteSettings: site } = useStaticQuery(QUERY)
-
   const metaDescription = description || site.description
 
   return (
