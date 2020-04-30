536aa7dd8a5b8fa80a46c60857259b30e1065739
Bryan Veloso <bryan@avalonstar.com>
2020-04-29 19:54:55 -0700

    chore: Edit gatsby-plugin-manifest configuration.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -47,13 +47,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Avalonstar`,
+        short_name: `Avalonstar`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1a1f23`,
+        theme_color: `#1a1f23`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
