53fe54866d519ee1a90ef8c5f744688224d75777
Bryan Veloso <bryan@avalonstar.com>
2020-05-01 10:40:49 -0700

    Fix: Style the Figure component.

--- web/src/components/Figure.tsx
+++ web/src/components/Figure.tsx
@@ -3,7 +3,7 @@
 
 import Img from 'gatsby-image'
 import { getFluidGatsbyImage } from 'gatsby-source-sanity'
-import { jsx } from 'theme-ui'
+import { jsx, Box } from 'theme-ui'
 
 import clientConfig from '../../client-config'
 
@@ -11,9 +11,11 @@ export default ({ node }) => {
   if (!node || !node.asset || !node.asset._id) return null
   const fluidProps = getFluidGatsbyImage(node.asset._id, { maxWidth: 1200 }, clientConfig.sanity)
   return (
-    <figure>
+    <figure sx={{ m: 0, mx: -24 }}>
       <Img fluid={fluidProps} alt={node.alt} />
-      <figcaption>{node.caption}</figcaption>
+      <Box sx={{ variant: 'structure.caption' }}>
+        <figcaption>{node.caption}</figcaption>
+      </Box>
     </figure>
   )
 }
