051759b1d6677562d0ab2112ed39fc5aebf4ee6a
Bryan Veloso <bryan@avalonstar.com>
2020-05-02 15:44:57 -0700

    fix: example.com -> avalonstar.com.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,7 +7,7 @@ const clientConfig = require('./client-config')
 
 const {
   NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://www.example.com',
+  URL: NETLIFY_SITE_URL = 'https://www.avalonstar.com',
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env
