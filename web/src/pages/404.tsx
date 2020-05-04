6dd3fdd8720fb1a6f56f60c96891b235a2a0db3f
Bryan Veloso <bryan@avalonstar.com>
2020-05-04 13:12:08 -0700

    chore: Axe unused layout and image components.

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { SEO } from '@/components'
-import Layout from '../components/layout'
+import { Layout } from '@/containers'
 
 const NotFoundPage = () => (
   <Layout>
