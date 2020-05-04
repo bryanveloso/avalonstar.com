5a71168ef15a6375a6e70441121f26c328a219de
Bryan Veloso <bryan@avalonstar.com>
2020-05-04 13:09:31 -0700

    fix: Remove an unneeded flexbox.

--- web/src/components/PageHeader.tsx
+++ web/src/components/PageHeader.tsx
@@ -2,15 +2,13 @@
 import { jsx, Box, Flex, Heading, Text } from 'theme-ui'
 
 const PageHeader = ({ title, subtitle }) => (
-  <Flex sx={{ py: 4, px: 4 }}>
-    <Box sx={{ flex: '1 1 auto' }}>
-      <Heading as="h1" sx={{ variant: 'text.hero', mb: 0 }}>
-        {title}
-        <span sx={{ color: 'main.avagreen' }}>.</span>
-      </Heading>
-      <Text sx={{ variant: 'text.subhero' }}>{subtitle}</Text>
-    </Box>
-  </Flex>
+  <Box sx={{ mb: 6 }}>
+    <Heading as="h1" variant="hero" sx={{ mb: 0 }}>
+      {title}
+      <span sx={{ color: 'main.avagreen' }}>.</span>
+    </Heading>
+    <Text variant="subhero">{subtitle}</Text>
+  </Box>
 )
 
 export default PageHeader
