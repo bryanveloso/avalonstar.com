d63ea291088ee9c7138971ec7e2d220bdf6b9ba9
Bryan Veloso <bryan@avalonstar.com>
2020-05-11 21:34:13 -0700

    fix: Lowercase the damn "S."

--- web/src/components/PortableText.tsx
+++ web/src/components/PortableText.tsx
@@ -1,10 +1,12 @@
 import BlockContent from '@sanity/block-content-to-react'
 import React from 'react'
 
-import serializers from '@/components/Serializers/Base'
+import serializers from '@/components/serializers/Base'
 
 import clientConfig from '../../client-config'
 
-const PortableText = ({ blocks }) => <BlockContent blocks={blocks} serializers={serializers} />
+const PortableText = ({ blocks }) => (
+  <BlockContent blocks={blocks} serializers={serializers} {...clientConfig.sanity} />
+)
 
 export default PortableText
