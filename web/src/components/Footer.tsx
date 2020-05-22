ef0f3eba72610cdee44e331e03c18cd323ca2b53
Bryan Veloso <bryan@avalonstar.com>
2020-05-21 22:02:26 -0700

    chore: Switch avalonSTAR in footer to PNG.

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
 import differenceInYears from 'date-fns/differenceInYears'
-import { jsx, Box, Flex, Grid, Link } from 'theme-ui'
+import { jsx, Box, Grid, Link } from 'theme-ui'
 
 import { useAuthorData, useSiteSettings } from '@/hooks'
 
-import Avalonstar from '@/images/avalonstar.svg'
+import Avalonstar from '@/images/avalonstar.png'
 import Facebook from '@/images/icon-facebook.svg'
 import GitHub from '@/images/icon-github.svg'
 import Instagram from '@/images/icon-instagram.svg'
@@ -19,27 +19,23 @@ const Footer = () => {
   const { title } = useSiteSettings()
 
   return (
-    <footer sx={{ bg: 'muted.dark' }}>
+    <footer>
       <Grid
         sx={{
           variant: 'layout.container',
           py: 6,
         }}
       >
-        <Grid
-          columns={['auto', 'auto 1fr']}
-          sx={{ pt: 6, borderTop: '1px solid', borderColor: 'muted.bluegrey' }}
-        >
+        <Grid columns={['auto', 'auto 1fr']} sx={{ pt: 6, borderTop: '1px solid', borderColor: 'muted.bluegrey' }}>
           <Box>
-            <Avalonstar sx={{ height: 48 }} />
+            <img src={Avalonstar} alt="logo" sx={{ height: 48 }} />
           </Box>
           <Grid gap={6} columns={['auto', '1fr 1fr']}>
             <Box sx={{ color: 'muted.lightbluegrey', fontSize: 1, lineHeight: '1.5rem' }}>
               <strong sx={{ color: 'white' }}>Avalonstar</strong> is the{' '}
               {differenceInYears(new Date(), new Date(2000, 9, 28))}
               -year-old personal website of Bryan Veloso: <em>content creator</em>,{' '}
-              <em>retired professional user interface designer</em>, and <em>compass of purpose</em>
-              .
+              <em>retired professional user interface designer</em>, and <em>compass of purpose</em>.
             </Box>
             <Grid gap={2} columns={[6, 2, 3]} sx={{ justifyContent: 'space-between' }}>
               <Box>
