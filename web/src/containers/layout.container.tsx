e085e65f839dc1646940f1ff4b2b18aa49cdc214
Bryan Veloso <bryan@avalonstar.com>
2020-05-28 15:47:43 -0700

    fix: Use <Global> to style the <html> tag.

--- web/src/containers/layout.container.tsx
+++ web/src/containers/layout.container.tsx
@@ -1,10 +1,12 @@
 /** @jsx jsx */
 import { motion, AnimatePresence } from 'framer-motion'
 import { jsx, Box, Flex } from 'theme-ui'
+import { Global } from '@emotion/core'
 
 import { Footer, Header } from '@/components'
 
 import './layout.css'
+import { Fragment } from 'react'
 
 const duration = 0.3
 const variants = {
@@ -26,31 +28,34 @@ const variants = {
 }
 
 const Layout = ({ children, location }) => (
-  <Box
-    sx={{
-      borderTop: '0.25rem solid',
-      borderTopColor: 'gradient.darkest',
-      boxShadow: 'inset 0 1px 0 #23292f',
-      width: '100vw',
-    }}
-  >
-    <Header />
-    <Flex sx={{ variant: 'layout.wrapper', minHeight: '100vh', flexDirection: 'column' }}>
-      <AnimatePresence initial={false}>
-        <motion.main
-          key={location.pathname}
-          variants={variants}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          sx={{ flexGrow: 1 }}
-        >
-          {children}
-        </motion.main>
-      </AnimatePresence>
-      <Footer />
-    </Flex>
-  </Box>
+  <Fragment>
+    <Global styles={() => ({ html: { backgroundColor: '#05070d' } })} />
+    <Box
+      sx={{
+        borderTop: '0.25rem solid',
+        borderTopColor: 'gradient.darkest',
+        boxShadow: 'inset 0 1px 0 #23292f',
+        width: '100vw',
+      }}
+    >
+      <Header />
+      <Flex sx={{ variant: 'layout.wrapper', minHeight: '100vh', flexDirection: 'column' }}>
+        <AnimatePresence initial={false}>
+          <motion.main
+            key={location.pathname}
+            variants={variants}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            sx={{ flexGrow: 1 }}
+          >
+            {children}
+          </motion.main>
+        </AnimatePresence>
+        <Footer />
+      </Flex>
+    </Box>
+  </Fragment>
 )
 
 export default Layout
