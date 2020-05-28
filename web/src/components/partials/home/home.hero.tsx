e915d85722b5347f4d8b510049783b47ac004663
Bryan Veloso <bryan@avalonstar.com>
2020-05-28 14:31:24 -0700

    chore: Placeholder Hero component.

---
+++ web/src/components/partials/home/home.hero.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx, Box, Link, Text } from 'theme-ui'
+
+const Hero = () => {
+  return <Box sx={{ height: 0 }} />
+}
+
+export default Hero
--- web/src/components/partials/home/index.ts
+++ web/src/components/partials/home/index.ts
@@ -1,3 +1,4 @@
 export { default as Company } from './home.company'
 export { default as EntryList } from './Home.EntryList'
+export { default as Hero } from './home.hero'
 export { default as QuoteList } from './Home.QuoteList'
