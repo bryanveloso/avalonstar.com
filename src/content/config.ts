867e09447f94b26e3475bbedf0d971acb193331f
Bryan Veloso <bryan@velo.so>
2024-07-14 10:17:55 -0700

    (pubDate -> date) and some formatting.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+    updatedDate: z.string().datetime().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
